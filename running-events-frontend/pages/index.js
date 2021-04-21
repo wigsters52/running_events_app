9f8b3f19be0005c46ebbae466133b0d457d20e2a
Ryan Wiggins <ryanwiggins52@gmail.com>
2021-04-21 15:22:27 -0400

    first commit

---
+++ running-events-frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function HomePage() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}

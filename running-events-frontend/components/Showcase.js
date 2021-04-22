0721142fc10faf28d95c9376c12ea606a9461f08
Ryan Wiggins <ryanwiggins52@gmail.com>
2021-04-22 13:03:08 -0400

    added showcase comp

---
+++ running-events-frontend/components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+    return (
+        <div className={styles.showcase}>
+            <h1>Welcome to the runner's party!</h1>
+            <h2>Find the best races for you!</h2>
+        </div>
+    )
+}

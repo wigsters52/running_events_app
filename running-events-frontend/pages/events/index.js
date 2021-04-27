63211a8312d5582108b797b99fe88b65cfa20226
Ryan Wiggins <ryanwiggins52@gmail.com>
2021-04-27 18:26:13 -0400

    adisplaying events

--- running-events-frontend/pages/events/index.js
+++ running-events-frontend/pages/events/index.js
@@ -1,10 +1,28 @@
 import Layout from '@/components/Layout'
+import {API_URL} from '@/config/index'
+import EventItem from 'components/EventItem'
+// import Pagination from 'components/Pagination'
 
+export default function EventsPage({events}) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
 
-export default function EventsPage() {
-    return (
-        <Layout>
-            <h1>My Events</h1>
-        </Layout>
-    )
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      
+    </Layout>
+  )
 }
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+  return {
+    props: {events},
+    revalidate: 1
+  }
+} 
